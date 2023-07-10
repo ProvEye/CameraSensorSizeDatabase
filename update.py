26f5c15aad2c041645ad3f9bf3e18a2e1f7200bd
evanokeeffe <eokeeffe@proveye.io>
2023-07-10 14:56:50 +0100

    added another ~3k cameras with sensor dimensions

---
+++ update.py
@@ -0,0 +1,22 @@
+
+originals = []
+with open("sensor_database.csv") as fp:
+    for line in fp.readlines():
+        parts = line.split(",")
+        print(parts)
+        newline = parts[1]+";"+parts[2]
+        originals.append(newline)
+
+latest = []
+with open("sensor_database2.csv") as fp:
+    for line in fp.readlines():
+        latest.append(line)
+
+oset = set(originals)
+lset = set(latest)
+
+nset = oset.union(lset)
+
+with open("sensor_database3.csv","w") as fp:
+    for line in sorted(nset):
+        fp.write(line)
